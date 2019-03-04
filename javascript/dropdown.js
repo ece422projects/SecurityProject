30b8689c9dbedcc548b77bd95512633d7d7f2ac0
StefanVidakovic <vidakovi@ualberta.ca>
2019-03-03 21:12:26 -0700

    Dropdown works

---
+++ javascript/dropdown.js
@@ -0,0 +1,16 @@
+function groupDropdown() {
+  var x = document.getElementById("groups");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('#group')) {
+    var dropdowns = document.getElementById("groups");
+    dropdowns.style.display = "none";
+  }
+}
